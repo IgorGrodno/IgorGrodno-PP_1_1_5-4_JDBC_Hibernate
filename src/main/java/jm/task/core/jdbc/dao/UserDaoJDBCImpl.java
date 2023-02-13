ac9a8a8156231ce355f7efff7a4d6f015abb1b5c
noobprofesor <noobprofesor@gmail.com>
2023-02-13 21:35:35 +0300

    refactor: add preparedStatement

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,52 +3,60 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    Util util = new Util();
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        String queryString = "CREATE TABLE IF NOT EXISTS `usersdb`.`users` (`id` INT NOT NULL AUTO_INCREMENT,`name` VARCHAR(45) NULL,`lastName` VARCHAR(45) NULL,`age` TINYINT(3) NULL,PRIMARY KEY (`id`));";
-        qeryStringExtcuter(queryString);
+        try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE IF NOT EXISTS `usersdb`.`users` (`id` INT NOT NULL AUTO_INCREMENT,`name` VARCHAR(45) NULL,`lastName` VARCHAR(45) NULL,`age` TINYINT(3) NULL,PRIMARY KEY (`id`));");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-        String queryString = "DROP TABLE IF EXISTS Users";
-        qeryStringExtcuter(queryString);
+        try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS Users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+
     public void saveUser(String name, String lastName, byte age) {
-        StringBuilder queryString= new StringBuilder();
-        queryString.append("INSERT INTO Users (name, lastName,age) VALUES (").
-                append("'").
-                append(name).
-                append("'").
-                append(",").
-                append("'").
-                append(lastName).
-                append("'").
-                append(",").
-                append(age).
-                append(");");
-        qeryStringExtcuter(queryString.toString());
+        String queryString = "INSERT INTO Users (name, lastName,age) VALUES (?,?,?)";
+        try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
+            PreparedStatement preparedStatement=connection.prepareStatement(queryString);
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,lastName);
+            preparedStatement.setByte(3,age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-        String queryString = "DELETE FROM Users WHERE Id=id";
-        qeryStringExtcuter(queryString);
+        String queryString = "DELETE FROM Users WHERE Id=?";
+        try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
+            PreparedStatement preparedStatement=connection.prepareStatement(queryString);
+            preparedStatement.setLong(1,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
         List<User> result = new ArrayList<User>();
-        Util util = new Util();
         try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Users");
             while (resultSet.next()) {
@@ -66,14 +74,8 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        String queryString = "DELETE FROM Users";
-        qeryStringExtcuter(queryString);
-    }
-
-    private void qeryStringExtcuter(String qeryString) {
-        Util util = new Util();
         try (Connection connection = util.getConnection(); Statement statement = connection.createStatement()) {
-            statement.execute(qeryString);
+            statement.execute("DELETE FROM Users");
         } catch (SQLException e) {
             e.printStackTrace();
         }
