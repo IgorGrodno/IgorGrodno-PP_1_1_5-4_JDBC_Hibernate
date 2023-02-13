67c12e5e5e25edd3fc04f577f1027f07ca00d3aa
noobprofesor <noobprofesor@gmail.com>
2023-02-13 23:38:48 +0300

    add Hibernate configuration

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
+
         userService.createUsersTable();
         userService.saveUser("name", "lastName", (byte) 1);
         User tmpUser = userService.getAllUsers().get(userService.getAllUsers().size() - 1);
@@ -24,11 +25,11 @@ public class Main {
         userService.saveUser("name3", "lastName3", (byte) 4);
         tmpUser = userService.getAllUsers().get(userService.getAllUsers().size() - 1);
         System.out.println("User с именем – " + tmpUser.getName() + " добавлен в базу данных");
-
         List<User> userList = userService.getAllUsers();
         for (User user : userList) {
             System.out.println(user.toString());
         }
+
         userService.cleanUsersTable();
         userService.dropUsersTable();
     }
